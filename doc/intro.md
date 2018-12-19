79affa4d5e9a8542320688dbd957c2793466c12d
axelksh <kulish.axel@gmail.com>
2018-12-19 19:39:49 +0200

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to personal-site
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/personal_site/core_test.clj
@@ -0,0 +1,7 @@
+(ns personal-site.core-test
+  (:require [clojure.test :refer :all]
+            [personal-site.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
